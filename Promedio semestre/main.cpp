d715897d56af362e30b37f92b349d72e8c495c34
AlvaNeedsFood <aalvarez.contact05@gmail.com>
2023-08-15 08:21:25 -0500

    Initial Commit

---
+++ Promedio semestre/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Promedio semestre
+//
+//  Created by Hope Alvarez on 15/08/23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
